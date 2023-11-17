6abfd29d1800149789d3e31b67b2c6429c56e4a7
India Grant <60491066+indiagrant@users.noreply.github.com>
2023-11-17 15:27:16 +0000

    added psuedocode

--- pages/blogs/[id].jsx
+++ pages/blogs/[id].jsx
@@ -1,11 +1,14 @@
-import blogData from "../../json/data.json"; // Update the path accordingly
+import blogData from "../../json/data.json";
 
-const YourComponent = ({ blogPost }) => {
-  // Find the blog post based on the id
+//create a functional component called BlogPostComponent
+//destructures the 'blogPost' prop from the props object.
+const BlogPostComponent = ({ blogPost }) => {
+  // checks if 'blogPost' is false (returns blog post not found)
   if (!blogPost) {
     return <p>Blog post not found</p>;
   }
 
+  //if true, the component returns the JSX for rednering the blog post content.
   return (
     <>
       <h1>{blogPost.heading}</h1>
@@ -19,15 +22,19 @@ const YourComponent = ({ blogPost }) => {
   );
 };
 
-export default YourComponent;
+export default BlogPostComponent;
 
+//get data from the JSON file
 export async function getServerSideProps({ params }) {
   const { id } = params;
 
+  //use the find array method to iterate through the blogPosts array and "find" the post based on the id.
+  //parseInt here parses the id (if its a string) into an integer. This is precautionary.
   const blogPost = blogData.blogPosts.find(
     (post) => post.id === parseInt(id, 10)
   );
 
+  //if id not found, return null
   if (!blogPost) {
     return {
       props: {
@@ -36,6 +43,7 @@ export async function getServerSideProps({ params }) {
     };
   }
 
+  //if found, return the data as a prop
   return {
     props: {
       blogPost,
